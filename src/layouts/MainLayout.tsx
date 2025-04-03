de8701a717684367d84a91351c7e47f43eb10ff7
Emmanuel Olubiyi <grggmmnl@gmail.com>
2025-04-03 02:39:16 +0100

    fix: minor fixes make component folders lowercase made footer background black to be consistent with the design

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
-import Footer from '../components/Footer';
-import NavBar from '../components/NavBar';
+import Footer from '../components/footer';
+import NavBar from '../components/navbar';
 
 interface LayoutProps {
   children: ReactNode;
